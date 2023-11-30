52113d63beeeff2c81ab52fac508b98eb905a44d
pavelbrosin <pavel.brosinys@gmail.com>
2023-11-30 14:51:47 +0300

    add cat + test, ver. 1.0

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,15 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld bean1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println("Переменные HelloWorld ссылаются на один обьект? "
+                + (bean1 == bean2));
+        System.out.println("Переменные Cat ссылаются на один обьект? "
+                + (cat1 == cat2));
     }
 }
